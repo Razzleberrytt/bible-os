64c3fbc8d2ab4f63391c8b8098c0b728cc31be26
Willie Randolph <RazzleberryT@gmail.com>
2026-08-03 15:27:12 -0400

    feat: expose USFM visible-text detection

--- bible_os/importers/__init__.py
+++ bible_os/importers/__init__.py
@@ -1,6 +1,12 @@
 """Source-adapter interfaces and implementations."""
 
 from .base import AdapterProbe, SourceAdapter, SourceRecord
-from .webp_usfm import WebpUsfmAdapter
+from .webp_usfm import WebpUsfmAdapter, extract_visible_text
 
-__all__ = ["AdapterProbe", "SourceAdapter", "SourceRecord", "WebpUsfmAdapter"]
+__all__ = [
+    "AdapterProbe",
+    "SourceAdapter",
+    "SourceRecord",
+    "WebpUsfmAdapter",
+    "extract_visible_text",
+]
